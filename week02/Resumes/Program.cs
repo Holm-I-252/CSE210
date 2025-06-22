349d4fd66c28beda70368a6d5784ab2564d5798d
Ian Holm <ianholm252@gmail.com>
2025-06-21 20:42:59 -0600

    Update for resumes assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+        myResume._jobs = new List<Job>();
+        // Adding jobs to the resume
+        myResume._jobs.Add(new Job { _company = "Google", _jobTitle = "Software Engineer", _startYear = 2015, _endYear = 2020 });
+        myResume._jobs.Add(new Job { _company = "Microsoft", _jobTitle = "Systems Analyst", _startYear = 2020, _endYear = 2023 });
+
+        myResume.DisplayResume();
     }
 }
