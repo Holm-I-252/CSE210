46c8b461358f0ef976dad30f714095cc44ad45ef
Ian Holm <ianholm252@gmail.com>
2025-06-28 17:28:11 -0600

    Added fractions assignment

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,64 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            _bottom = bottom;
+        }
+        else
+        {
+            Console.WriteLine("Bottom cannot be zero.");
+        }
+    }
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        if (_bottom != 0)
+        {
+            return (double) _top / _bottom;
+        }
+        else
+        { Console.WriteLine("Cannot divide by zero.");
+            return 0.0;
+        }
+    }      
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction_1 = new Fraction();
+        Fraction fraction_2 = new Fraction(3);
+        Fraction fraction_3 = new Fraction(1, 2);
+
+        fraction_1.SetTop(5);
+        fraction_1.SetBottom(10);
+
+        Console.WriteLine($"Fraction 1: {fraction_1.GetFractionString()}");
+        Console.WriteLine($"Deciaml 1: {fraction_1.GetDecimalValue()}");
     }
 }
